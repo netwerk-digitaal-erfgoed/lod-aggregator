90384d96072399cc8d74ebdff851ff6478fe27e8
Enno <ennomeijers@protonmail.com>
2020-06-08 20:14:31 +0200

    Some tweaks for rdf2edm

--- crawler/src/assemble/rdf2edm.sh
+++ crawler/src/assemble/rdf2edm.sh
@@ -6,5 +6,4 @@ do
   export CLASSPATH=$CLASSPATH:$jar
 done
 export CLASSPATH=$CLASSPATH
-
-java -Djava.util.logging.config.file="logging.properties" -Dsun.net.inetaddr.ttl=0 -Xmx4G -cp classes:$CLASSPATH eu.europeana.commonculture.lod.rdf2edmxml.Rdf2EdmCl $*
+java -Djava.util.logging.config.file="logging.properties" -Dsun.net.inetaddr.ttl=0 $JVM_ARGS -cp classes:$CLASSPATH eu.europeana.commonculture.lod.rdf2edmxml.Rdf2EdmCl $*
